4e5640c085b3566b5ec021aa13a02caf867295e5
Iftakher99 <19tonmoy99@gmail.com>
2021-01-28 01:02:33 +0600

    addded some javascript function

--- pigeon.js
+++ pigeon.js
@@ -1,3 +1,4 @@
+//login button event handler
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function () {
   const loginArea = document.getElementById("login-area");
@@ -5,3 +6,31 @@ loginBtn.addEventListener("click", function () {
   const transectionArea = document.getElementById("transection-area");
   transectionArea.style.display = "block";
 });
+//Deposit button event handler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function () {
+  const depositAmount = document.getElementById("depositAmount").value;
+  const totalDeposit = parseFloat(depositAmount);
+  //taking deposit value
+  // const currentDeposit = document.getElementById("current-deposit").innerText;
+  // const currentDepositNumber = parseFloat(currentDeposit);
+  // const totalDeposit = depositNumber + currentDepositNumber;
+  // document.getElementById("current-deposit").innerText = totalDeposit;
+  updateSpanText("current-deposit", totalDeposit);
+
+  //Working on current-balance
+  updateSpanText("current-balance", totalDeposit);
+
+  document.getElementById("depositAmount").value = "";
+  // const currentBalance = document.getElementById("current-balance").innerText;
+  // const currentBalanceNumber = parseFloat(currentBalance);
+  // const totalMainBalance = currentBalanceNumber + totalDeposit;
+  // document.getElementById("current-balance").innerText = totalMainBalance;
+});
+
+function updateSpanText(id, totalDeposit) {
+  const currentBalance = document.getElementById(id).innerText;
+  const currentBalanceNumber = parseFloat(currentBalance);
+  const totalMainBalance = currentBalanceNumber + totalDeposit;
+  document.getElementById(id).innerText = totalMainBalance;
+}
